6f316ed2def8040edafcc5ab56d5d7066bc57702
technolog <viktor@allz.su>
2019-06-11 23:00:18 +0300

    Generator:prepare

---
+++ src/components/Generator.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <dm-button color="black">Generate new address</dm-button>
+
+        <div class="result-generate">
+            <h3>Private key:</h3>
+            <h3>Address:</h3>
+            <h3>HEX:</h3>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
