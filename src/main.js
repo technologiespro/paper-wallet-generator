f41f7d228b37aef83e0ada75ff7fd0b8a5a4cf19
technolog <viktor@allz.su>
2019-06-12 01:59:00 +0300

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css';
 import '@/css/styles.css';
 
 Vue.use(VueDarkMode);
