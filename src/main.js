81560b2be25ca3ad79939b8ba7006c62e64a4a41
technolog <viktor@allz.su>
2019-06-11 22:42:14 +0300

    init:VueDarkMode

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import VueDarkMode from "vuedarkmode";
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.use(VueDarkMode);
 
 Vue.config.productionTip = false
 
