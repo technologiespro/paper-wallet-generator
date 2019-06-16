5e61728d10269a916c5176c441abf358ca3ae524
technolog <viktor@allz.su>
2019-06-17 02:54:50 +0300

    method:downloadTxt

--- src/components/DownloadTxt.vue
+++ src/components/DownloadTxt.vue
@@ -1,11 +1,27 @@
 <template>
+  <div>
 
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "DownloadTxt"
+  export default {
+    name: "DownloadTxt",
+    methods: {
+      downloadTxt(filename, text) {
+        let element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+      }
     }
+  }
 </script>
 
 <style scoped>
