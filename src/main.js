098827b6ab556cc3e92f590c778b4b4230e27b17
technolog <viktor@allz.su>
2019-06-26 06:30:34 +0300

    eventBus glob

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Clipboard from 'v-clipboard'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import eventBus from '@/plugins/event-bus'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '@/css/styles.css';
@@ -11,7 +12,8 @@ import '@/css/styles.css';
 Vue.use(VueDarkMode);
 Vue.use(Clipboard);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$eventBus = eventBus;
 
 new Vue({
   router,
