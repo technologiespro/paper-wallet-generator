18e1810d3e62e7c34383ee50864679463a7204eb
technolog <technologies.bro@gmail.com>
2020-05-03 06:00:45 +0300

    'Deploy'

--- src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <router-link to="/"><dm-button color="black" class="mb-2">RETURN</dm-button></router-link>
-    <h1>Paper Wallet Generator offline</h1>
-    <p>
-      Designed for simple creation of addresses of popular cryptocurrencies
-    </p>
-  </div>
-</template>
