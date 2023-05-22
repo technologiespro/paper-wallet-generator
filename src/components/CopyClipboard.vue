cbad9ef77030ccb4555a755202626eeee58150fb
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-05-22 17:22:56 +0300

    cp

---
+++ src/components/CopyClipboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <span>
+      <img title="Copy clipboard" style="cursor: pointer;" src="static/svg/copy.svg" width="20px" class="clipboard"
+           v-clipboard="() => value"
+           v-clipboard:success="clipboardSuccessHandler"
+      />
+    </span>
+</template>
+
+<script>
+  import eventBus from '@/plugins/event-bus'
+  export default {
+    name: "CopyClipboard",
+    props: {
+      value: String
+    },
+    methods: {
+      clipboardSuccessHandler({value, event}) {
+        eventBus.emit('copied');
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
